683ba4960040b324ee72c48a7b153646c80d0e21
Ivan Doroshenko <ivan@dev-branch.com>
2020-06-02 10:53:02 +0300

    OI-13 #comment Tested auto-comments.

--- scripts/install-git-hooks.sh
+++ scripts/install-git-hooks.sh
@@ -2,7 +2,7 @@
 
 docroot=$(pwd)
 
-# Add git hooks to both repositories.
+# Add git hooks to our both git repositories.
 cp scripts/git_hooks/* .git/hooks/
 cp scripts/git_hooks/* web/profiles/contrib/idea/.git/hooks/
 
