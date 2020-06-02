2a8c56f5838ddd293eaba3a0aa4512c09cc3f4e5
Ivan Doroshenko <i.doroshenko@dev-branch.com>
2020-06-02 10:30:29 +0300

    OI-13 #comment Added git commit validation message.

---
+++ scripts/install-git-hooks.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docroot=$(pwd)
+
+# Add git hooks to both repositories.
+cp scripts/git_hooks/* .git/hooks/
+cp scripts/git_hooks/* web/profiles/contrib/idea/.git/hooks/
+
+# Remove sample files.
+cd scripts/git_hooks
+for file in *; do
+  sample="$file.sample"
+  rm -f "$docroot/.git/hooks/$sample"
+  rm -f "$docroot/web/profiles/contrib/idea/.git/hooks/$sample"
+done
